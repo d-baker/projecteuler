2b902657b1c9186e31f0819ae1c3df8f062addfd
D Baker <d-baker@server.fake>
2014-01-19 15:07:25 +1100

    cleaned up + added comments

--- 5.py
+++ 5.py
@@ -8,16 +8,15 @@ def multiples():
 		
 		for divisor in range (20, 0, -1):
 		
-			if n % divisor == 0:
-				if divisor == 1:
+			if n % divisor == 0: # if we pass this test then the number is divible by 20
+				if divisor == 1: # if we reach 1 and it's still divisible by 20, save the number
 					result.append(n)
 					break;
-				else:
-					pass;
 				
 			else:
 				break; # that number's no good, jump back to the outer loop and start trying the next number
 		
+		# if we got a result, print it and end the loop
 		if result:	
 			print result
 			break;
