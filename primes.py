e5c37a167119bf3f1d7815aa8684bf728c76edfe
dbaker <d-_-baker@hotmail.com>
2014-03-27 22:11:56 +1100

    refactored primes code - slightly more efficient(?) + readable now

--- primes.py
+++ primes.py
@@ -1,17 +1,16 @@
 def primes(num_nums):
-	primes=[2]
-	
+	primes=[2] # initialise our primes list with 2
+
 	for potential_prime in range(3, num_nums, 2): # step by 2 to avoid even numbers
-		
-		for divisor in range(2, potential_prime+1): # check each number against every divisor up to the number itself
-			
-			if potential_prime % divisor != 0:
-				if divisor==potential_prime -1 : # if we reach this, the number isn't divisible by any of the divisors we've tried - so save it!
+
+		for divisor in range(2, potential_prime/2): # check each number against every plausible divisor
+
+			if potential_prime % divisor == 0:
+				continue
+			elif divisor==(potential_prime/2) - 1: # if we reach this, the number isn't divisible by any of the divisors we've tried - so save it!
 					primes.append(potential_prime)
-			else:
-				break
 
 	print primes
 
 if __name__ == "__main__":
-	primes(100)
+	primes(20000)
